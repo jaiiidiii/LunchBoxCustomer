5e725f664a13e988e310f2d53bc8dd0d53433e2f
khizarsulehria <khizar.sulehri.ks@gmail.com>
2018-08-01 03:15:32 +0500

    Dishes Listing in PlaceOrderActivity Done

--- app/src/main/java/com/jayzonsolutions/LunchBox/PlaceOrderActivity.java
+++ app/src/main/java/com/jayzonsolutions/LunchBox/PlaceOrderActivity.java
@@ -2,23 +2,38 @@ package com.jayzonsolutions.LunchBox;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.location.SettingInjectorService;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
@@ -26,10 +41,13 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.Places;
 import com.google.android.gms.location.places.ui.PlacePicker;
+import com.jayzonsolutions.LunchBox.Fragments.DetailFragment;
 import com.jayzonsolutions.LunchBox.Service.OrderService;
 import com.jayzonsolutions.LunchBox.model.ApiResponse;
 import com.jayzonsolutions.LunchBox.model.Cart;
 import com.jayzonsolutions.LunchBox.model.CartItem;
+import com.jayzonsolutions.LunchBox.model.Categories;
+import com.jayzonsolutions.LunchBox.model.FoodmakerDishes;
 import com.jayzonsolutions.LunchBox.model.Order;
 import com.jayzonsolutions.LunchBox.model.OrderDish;
 
@@ -42,6 +60,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import retrofit2.Call;
@@ -58,10 +77,66 @@ public class PlaceOrderActivity extends AppCompatActivity implements
     private TextView tvPlaceDetails;
     private OrderService orderService;
 
+
+    //
+    private RecyclerView recyclerView;
+    private RecycleAdapter_AddProduct mAdapter;
+    private Categories categories;
+
+    private Map<Integer,Double> orderdishes;
+    private Map<Integer,CartItem> cartItemMap;
+    Animation startAnimation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_placeorder);
+
+        /**
+         *
+         */
+        //create this screen orderDishes list
+        orderdishes = new HashMap<>();
+        cartItemMap = new HashMap<>();
+
+        startAnimation = AnimationUtils.loadAnimation(PlaceOrderActivity.this, R.anim.bounce);
+        Set<Map.Entry<Integer, CartItem>> entrySet = Cart.orderdishes.entrySet();
+        ArrayList<CartItem> listOfEntry = new ArrayList<CartItem>(Cart.orderdishes.values());
+        System.out.println(listOfEntry);
+
+
+        //
+        //foodmakerDishesList = new ArrayList<>();
+        categories = new Categories();
+        categories.productsArrayList = new ArrayList<>();
+        mAdapter = new PlaceOrderActivity.RecycleAdapter_AddProduct(PlaceOrderActivity.this, listOfEntry);
+        recyclerView = findViewById(R.id.cartitemrecyclerview);
+
+
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(PlaceOrderActivity.this);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+
+
+
+
+
+
+
+
+
+        /**
+         *
+         */
+
+
+
+
+
+
+
 
         if(Constant.customer == null){
             Intent intent = new Intent(PlaceOrderActivity.this,signin.class);
@@ -177,63 +252,6 @@ public class PlaceOrderActivity extends AppCompatActivity implements
 
 
 
-
-
-
-  /*
-                String date = txtDate.getText().toString();
-                String time = txtTime.getText().toString();
-                String dtStart = "2010-10-15T09:27:37Z";
-                Date datenew = null;
-                SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
-                try {
-                    datenew = format.parse(dtStart);
-                    System.out.println("Dtae ===> "+datenew);
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-        Order order = new Order();
-        order.setOrderCustomerId(10);
-        order.setOrderShipmentAddress("Ceaser Tower");
-        order.setOrderDate(datenew);
-
-        order.setOrderDeliverDate(datenew);
-        order.setOrderTotalAmount(102);
-        order.setOrderStatus(1);
-        order.setFoodmakerId(8);
-        List<OrderDish> orderDishes = new ArrayList<>();
-        OrderDish orderDish = new OrderDish();
-        orderDish.setDishId(27);
-        orderDish.setQuantity(2.00);
-
-        OrderDish orderDish1 = new OrderDish();
-        orderDish.setDishId(23);
-        orderDish.setQuantity(5.00);
-        orderDishes.add(orderDish1);
-        order.setOrderdishes(orderDishes);
-
-
-        orderService.placeOrder(order).enqueue(new Callback<ApiResponse>() {
-            @Override
-            public void onResponse(Call<ApiResponse> call, Response<ApiResponse> response) {
-
-                Toast.makeText(PlaceOrderActivity.this,"true json",Toast.LENGTH_LONG).show();
-
-           //     System.out.println(response.body().toString());
-
-
-
-            }
-
-            @Override
-            public void onFailure(Call<ApiResponse> call, Throwable t) {
-                Toast.makeText(PlaceOrderActivity.this,"failed json ",Toast.LENGTH_LONG).show();
-
-            }
-        });
-
-
-*/
 
 
 
@@ -359,6 +377,205 @@ public class PlaceOrderActivity extends AppCompatActivity implements
                 tvPlaceDetails.setText(stBuilder.toString());
             }
         }
+    }
+
+
+
+
+    //
+    public class RecycleAdapter_AddProduct extends RecyclerView.Adapter<PlaceOrderActivity.RecycleAdapter_AddProduct.MyViewHolder> {
+
+        Context context;
+        boolean showingFirst = true;
+        int recentPos = -1;
+        private List<FoodmakerDishes> foodmakerDishesList;
+        private List<CartItem> cartItems;
+
+
+        RecycleAdapter_AddProduct(Context context, List<CartItem> cartItems) {
+            this.cartItems = cartItems;
+            this.context = context;
+        }
+
+        void setFoodmakerDishesList(List<FoodmakerDishes> foodmakerDishesList) {
+            this.foodmakerDishesList = foodmakerDishesList;
+            notifyDataSetChanged();
+        }
+        void setCartItems(List<CartItem> cartItems) {
+            this.cartItems = cartItems;
+            notifyDataSetChanged();
+        }
+
+        @NonNull
+        @Override
+        public PlaceOrderActivity.RecycleAdapter_AddProduct.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_product, parent, false);
+
+
+            return new PlaceOrderActivity.RecycleAdapter_AddProduct.MyViewHolder(itemView);
+
+
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+        @Override
+        public void onBindViewHolder(@NonNull final PlaceOrderActivity.RecycleAdapter_AddProduct.MyViewHolder holder, final int position) {
+//            Products movie = productsList.get(position);
+
+            holder.title.setText(""+cartItems.get(position).getFoodmakerDishes().getName());
+            holder.price.setText(""+cartItems.get(position).getFoodmakerDishes().getPrice());
+            //  holder.price.setText(categories.getProductsArrayList().get(position).getPrice());
+            holder.quantityTxt.setText(""+cartItems.get(position).getQuantity());
+            //   holder.quantityTxt.setText(categories.getProductsArrayList().get(position).getQuantity() + "");
+
+
+            holder.quantity = 1;
+            Double d = cartItems.get(position).getQuantity();
+            holder.quantity = d.intValue();
+            //   holder.quantity = categories.getProductsArrayList().get(position).getQuantity();
+            Double totalPrice = holder.quantity * cartItems.get(position).getFoodmakerDishes().getPrice();
+
+
+            String imagePath = ((cartItems.get(position).getFoodmakerDishes().getImagepath() != null)?cartItems.get(position).getFoodmakerDishes().getImagepath():"http://localhost:8080/images/user_na.jpg");
+
+
+            Glide.with(context).load(ApiUtils.BASE_URL+(imagePath.substring(21))).
+                    apply(RequestOptions.
+                            centerCropTransform().fitCenter().
+                            diskCacheStrategy(DiskCacheStrategy.ALL)).
+                    into(holder.image);
+
+/*
+           Glide.with(context).load(ApiUtils.BASE_URL+"images/es2.jpg").
+                    apply(RequestOptions.
+                            centerCropTransform().fitCenter().
+                            diskCacheStrategy(DiskCacheStrategy.ALL)).
+                    into(holder.image);
+*/
+
+            if (position == recentPos) {
+                Log.e("pos", "" + recentPos);
+                // start animation
+                holder.quantityTxt.startAnimation(startAnimation);
+
+            } else {
+                holder.quantityTxt.clearAnimation();
+
+            }
+
+
+            if (holder.quantity > 0) {
+                holder.quantityTxt.setVisibility(View.VISIBLE);
+                holder.llMinus.setVisibility(View.VISIBLE);
+            } else {
+                holder.quantityTxt.setVisibility(View.GONE);
+                holder.llMinus.setVisibility(View.GONE);
+            }
+
+
+
+            //       categories.getProductsArrayList().get(position).setPriceAsPerQuantity("" + totalPrice);
+            cartItems.get(position).getFoodmakerDishes().getDish().setDishPriceAsPerQuantity(" "+totalPrice);
+
+            holder.llPlus.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    if (holder.quantity < 10) {
+                        int foodmakerdishId = cartItems.get(position).getFoodmakerDishes().getFoodmakerDishesId();
+
+                        recentPos = position;
+                        holder.quantity = holder.quantity + 1;
+                        cartItems.get(position).getFoodmakerDishes().getDish().setDishQuantity(holder.quantity);
+                        //   categories.getProductsArrayList().get(position).setQuantity(holder.quantity);
+                        cartItems.get(position).getFoodmakerDishes().getDish().setDishPriceAsPerQuantity("" + holder.quantity * cartItems.get(position).getFoodmakerDishes().getPrice());
+                        //    categories.getProductsArrayList().get(position).setPriceAsPerQuantity("" + holder.quantity * Integer.parseInt(categories.getProductsArrayList().get(position).getPrice()));
+
+                        holder.quantityTxt.setText("" + holder.quantity);
+
+                        double quan = (double) holder.quantity;
+                    //    orderdishes.put(foodmakerdishId,quan);
+                    //    Constant.orderdishes.put(foodmakerdishId,quan);
+                       int foodmakerId =  cartItems.get(position).getFoodmakerId();
+                    //    Constant.foodmakerdishes.put(foodmakerId,orderdishes);
+
+
+                        /**
+                         * cart item
+                         */
+
+
+                        cartItemMap.put(foodmakerdishId,new CartItem(foodmakerdishId,cartItems.get(position).getFoodmakerDishes(),quan));
+                        Cart.orderdishes.put(foodmakerdishId,new CartItem(foodmakerdishId,cartItems.get(position).getFoodmakerDishes(),quan));
+                        Cart.foodmakerdishes.put(foodmakerId,cartItemMap);
+
+                    }
+
+
+                    notifyDataSetChanged();
+
+                }
+            });
+
+
+            holder.llMinus.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    if (holder.quantity > 0 && holder.quantity <= 10) {
+
+                        recentPos = position;
+
+                        holder.quantity = holder.quantity - 1;
+                        cartItems.get(position).getFoodmakerDishes().getDish().setDishQuantity(holder.quantity);
+                        //        categories.getProductsArrayList().get(position).setQuantity(holder.quantity);
+                        cartItems.get(position).getFoodmakerDishes().getDish().setDishPriceAsPerQuantity("" + holder.quantity * cartItems.get(position).getFoodmakerDishes().getPrice());
+                        //          categories.getProductsArrayList().get(position).setPriceAsPerQuantity("" + holder.quantity *
+                        //              Integer.parseInt(categories.getProductsArrayList().get(position).getPrice()));
+
+                        holder.quantityTxt.setText("" + holder.quantity);
+
+
+                    }
+
+                    notifyDataSetChanged();
+
+                }
+            });
+
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return cartItems.size();
+        }
+
+        class MyViewHolder extends RecyclerView.ViewHolder {
+
+
+            ImageView image;
+            TextView title;
+            TextView price;
+            TextView quantityTxt;
+            int quantity;
+            private LinearLayout llMinus, llPlus;
+
+
+            MyViewHolder(View view) {
+                super(view);
+
+                image = view.findViewById(R.id.imageProduct);
+                title = view.findViewById(R.id.titleProduct);
+                price = view.findViewById(R.id.price);
+                quantityTxt = view.findViewById(R.id.quantityTxt);
+                llPlus = view.findViewById(R.id.llPlus);
+                llMinus = view.findViewById(R.id.llMinus);
+            }
+
+        }
+
     }
 
 }
